620c5d7e082c205afdf02d40eef421cfb3f40118
drinaissante <victoriaaldrin812@gmail.com>
2025-08-11 21:15:49 +0800

    Added galaxy effect on Hero.jsx.

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,24 +1,24 @@
 import TextType from "../extras/TextTyping.jsx"
-import Particles from "../extras/Particles.jsx";
 
+import Galaxy from "../extras/Galaxy.jsx"
 
 function Hero() {
     return (
     <section 
         id="home" 
-        className="flex flex-col items-center justify-center min-h-screen bg-gradient-to-br from-green-800 via-green-900 to-gray-200">
+        className="flex flex-col items-center justify-center min-h-screen bg-gradient-to-b from-black via-green-950 to-green-700">
             
-            <Particles
-                className="fixed inset-0 w-full h-full z-0"
+            {/* <Particles
                 particleColors={['#ffffff', '#ffffff']}
                 particleCount={200}
-                particleSpread={10}
+                particleSpread={9}
                 speed={0.1}
                 particleBaseSize={120}
                 moveParticlesOnHover={true}
                 alphaParticles={true}
                 disableRotation={false}
-            />
+            /> */}
+
         <div className="">
             {/* <GridDistortion
                 imageSrc={bg}
@@ -28,6 +28,8 @@ function Hero() {
                 relaxation={0.9}
             /> */}
         </div>
+
+
 
         <TextType 
             text={["Text typing effect", "SUP", "Happy coding!"]}
@@ -48,12 +50,21 @@ function Hero() {
         </GlitchText> */}
         
 
-        <h1 className="text-5xl font-bold mb-4 bg-gradient-to-r from-green-400 via-green-600 to-green-900 bg-clip-text text-transparent">
+        <h1 className="text-5xl font-bold mb-4 bg-gradient-to-t from-white via-green-600 to-green-900 bg-clip-text text-transparent">
             Welcome to CompSciety!
         </h1>
-        <p className="text-xl mb-8 bg-gradient-to-r from-green-300 to-green-700 bg-clip-text text-transparent">
+        <p className="text-xl mb-8 bg-gradient-to-t from-white to-green-700 bg-clip-text text-transparent">
             Background here.
         </p>
+        
+        <Galaxy 
+            className="absolute w-full h-[800px]"
+            mouseRepulsion={false}
+            mouseInteraction={true}
+            density={1}
+            saturation={0.3}
+            hueShift={100}
+        />
   </section>
     )
 }
