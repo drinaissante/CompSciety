ede70301876ac48179363b268e671e960c3794bd
drinaissante <victoriaaldrin812@gmail.com>
2025-09-21 00:32:59 +0800

    Changed to white font (not final)

--- src/components/Events.jsx
+++ src/components/Events.jsx
@@ -1,6 +1,8 @@
 import Announcement from "../models/Announcement.jsx";
 import Event from "../models/Event.jsx";
 
+import MotionDiv from "./MotionDiv.jsx";
+
 const events = [
     {
         name: "Event 1",
@@ -41,10 +43,10 @@ function Events() {
     return (
         <main id="events" className="flex flex-col lg:flex-row justify-center p-10 gap-4">
             {/* Events Column */}
-            <div className="lg:w-1/2 text-2xl text-black font-bold">
+            <div className="lg:w-1/2 text-2xl text-white font-bold">
                 Events
 
-                <div className="mt-5 text-xl flex flex-col justify-between gap-5 h-90">
+                <MotionDiv className="mt-5 text-xl flex flex-col justify-between gap-5 h-90">
                     {events.map((event, index) => (
                         <div key={index} className="flex rounded-xl overflow-hidden transform transition duration-300 hover:scale-101 shadow-xl/15">
                             <div className="bg-[#0e6b11] w-4 rounded-l-xl"></div>
@@ -53,11 +55,11 @@ function Events() {
                             </div>
                         </div>
                     ))}
-                </div>
+                </MotionDiv>
             </div>
 
             {/* Announcement Column */}
-            <div className="lg:w-1/4 text-2xl text-black font-bold mt-10 lg:mt-0 lg:min-w-1/4">
+            <MotionDiv transition={{ duration: 1, ease: "easeOut" }} className="lg:w-1/4 text-2xl text-white font-bold mt-10 lg:mt-0 lg:min-w-1/4">
                 Announcements
 
                 <div className="bg-[#5e936c] text-xl mt-5 rounded-2xl flex flex-col gap-3 p-4 transform transition duration-300 hover:scale-101 shadow-xl/15">
@@ -70,7 +72,7 @@ function Events() {
                         </div>
                     ))}
                 </div>
-            </div>
+            </MotionDiv>
         </main>
     );
 }
