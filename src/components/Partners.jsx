9b5493ce7c4b5ae3cf7dab2ebd76875fd099ea89
drinaissante <victoriaaldrin812@gmail.com>
2025-09-21 20:25:52 +0800

    Fixed padding and added BLISS

--- src/components/Partners.jsx
+++ src/components/Partners.jsx
@@ -8,9 +8,9 @@ import {motion, AnimatePresence} from "framer-motion";
 
 import cs_wizards from "../assets/CS_WIZARDS.jpg"
 import jpcs from "../assets/jpcs.png"
-import mascot from "../assets/mascot.png"
 import thecrunch from "../assets/thecrunch.png"
 import swits from "../assets/SWITS.png"
+import bliss from "../assets/BLISS.png"
 
 import MotionDiv from "../components/MotionDiv.jsx"
 
@@ -26,10 +26,10 @@ const partners = [
       "To unite and commit the youth to: technical and leadership excellence; fostering among themselves, lasting friendship, and constructive cooperation in genuine love of God and country.",
     vision: "",
     url: "https://www.facebook.com/JPCSCEUMLLS",
-    college: "CEU Malolos - CIT"
+    college: "(CEU Malolos) - CIT"
   },
   { name: "Society for the Welfare of Information Technology Students (SWITS) ", logoSrc: swits, mission: "The Society for the Welfare of Information Technology Students (SWITS) of the College of Information and Communications Technology (CICT) is a college-based organization founded in 2001. SWITS focuses its goals on addressing the needs of our IT students and anchors its activities on the organization's passion, mission, and vision. SWITS became recognized for their projects and yearly events; including Enablement Seminars, IT Mentorship and IT Congress.", vision: "", url: "https://www.facebook.com/SWITS.org", college: "CICT" },
-  { name: "BLISS", logoSrc: mascot, mission: "", vision: "", url: "https://www.facebook.com/BLISSorgCICT", college: "CICT" },
+  { name: "BLISS", logoSrc: bliss, mission: "The Brigade of Library and Information Science Students Organization (BLISS) empower future information specialists. BLISS is a college-based organization at Bulacan State University under the College of Information and Communications Technology, an organization for Bachelor of Library and Information Science (BLIS) students, committed to empower future information professionals to bridge gaps, foster unity, and strengthen the access to information resources. Through students’ collective efforts, we aim to connect people with knowledge, ensuring that libraries, archives, and information centers remain vibrant and relevant.", vision: "", url: "https://www.facebook.com/BLISSorgCICT", college: "CICT" },
 ];
 
 function HoverPreview({ bb, partner }) {
@@ -43,10 +43,18 @@ function HoverPreview({ bb, partner }) {
     if (!mounted || !bb || !partner || !document?.body) return null;
 
     const cardWidth = 700;
-    const left= bb.left + bb.width / 2 - cardWidth / 2 + window.scrollX;
+    const left = bb.left + bb.width / 2 - cardWidth / 2 + window.scrollX;
     
     const cardHeight = 220; // estimate or measure
-    let top = bb.top - cardHeight - 12 + window.scrollY;
+
+    let top = bb.top - cardHeight - 40 + window.scrollY;
+
+    if (partner.mission.length > 200) {
+        top -= 30;
+    } else {
+        top += 90;
+    }
+
 
     return createPortal(
         <AnimatePresence>
@@ -74,7 +82,7 @@ function HoverPreview({ bb, partner }) {
                     className="w-full h-36 object-contain bg-[#1a1a1a]"
                 />
 
-                <div className="p-4 text-white text-center max-h-60 overflow-y-auto">
+                <div className="p-4 text-white text-center overflow-y-auto max-h-full">
                     <h3 className="font-bold text-lg"> {partner.name} - {partner.college} </h3>
 
                     <p className="text-sm mt-1"> {partner.mission || ""} </p>
@@ -115,37 +123,42 @@ function Partners() {
 
             <MotionDiv
                 id="partners"
-                className="mt-20 mb-10 justify-self-center sm:w-[30%] w-[80%] h-auto pb-20 rounded-4xl shadow-[0_0_10px_5px_rgba(0,255,0,0.2)] overflow-visible"
+                className="mt-20 mb-10 justify-self-center sm:w-[30%] w-[80%] h-auto rounded-4xl shadow-[0_0_10px_5px_rgba(0,255,0,0.2)] overflow-visible"
             >
                 <Swiper
                     modules={[Autoplay]}
                     spaceBetween={30}
-                    slidesPerView={2.5}
+                    slidesPerView={2}
                     autoplay={{ delay: 4000, disableOnInteraction: false }}
                     speed={3000}
                     onSwiper={(swiper) => (swiperRef.current = swiper)}
+                    loop={true}
+                    loopPreventsSliding={true}
                 >
                     {partners.map((partner, i) => (
-
-                        <SwiperSlide key={i} className="overflow-visible">
-
+                        <SwiperSlide key={i}>
                             <div 
-                                className="relative group flex flex-col items-center text-center"
+                                className="relative group flex flex-col items-center text-center pb-5"
                                 onMouseEnter={(e) => handleMouseEnter(e, partner)}
                                 onMouseLeave={handleMouseLeave}
                             >
-
+                                
                                 <a
                                     href={partner.url}
                                     target="_blank"
                                     rel="noopener noreferrer"
+                                    className="select-none"
                                 >
-                                    <img
-                                        src={partner.logoSrc}
-                                        alt={partner.name}
-                                        className="mt-15 h-16 w-16 object-contain border-4 rounded-full mx-auto cursor-pointer transition-transform duration-300 group-hover:scale-105"
-                                    />
-                                    <span className="mt-2 text-center">{partner.name}</span>
+                                    <div className="flex flex-col items-center text-center">
+                                        <img
+                                            src={partner.logoSrc}
+                                            alt={partner.name}
+                                            className="mt-15 h-16 w-16 object-contain border-4 rounded-full cursor-pointer transition-transform duration-300 group-hover:scale-105"
+                                            draggable="false"
+                                        />
+
+                                        <span className="block mt-2 text-center">{partner.name}</span>
+                                    </div>
                                 </a>
                             </div>
 
@@ -153,12 +166,13 @@ function Partners() {
 
                     ))}
                 </Swiper>
+                
+                {hover && (
+                    <HoverPreview bb={hover.bb} partner={hover.partner} />
+                )}
 
             </MotionDiv>
 
-            {hover && (
-                <HoverPreview bb={hover.bb} partner={hover.partner} />
-            )}
 
         </div>
     )
