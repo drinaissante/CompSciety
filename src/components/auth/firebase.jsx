9f2499abecee6baae6e570df0f3c36edad55a0ad
drinaissante <victoriaaldrin812@gmail.com>
2025-09-23 19:59:45 +0800

    Added .env.local if needed

--- src/components/auth/firebase.jsx
+++ src/components/auth/firebase.jsx
@@ -1,6 +1,20 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth"
 
+/*
+    NOTE: THIS DOES NOT WORK SA GITHUB PAGES. This should work on vercel. pero okay lang naman daw na publicly available to (frontend) and kahit sa github repo.
+
+const firebaseConfig = {
+  apiKey: import.meta.env.FIREBASE_API_KEY,
+  authDomain: import.meta.env.FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.PROJECT_ID,
+  storageBucket: import.meta.env.STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.MESSAGING_SENDER_ID,
+  appId: import.meta.env.APP_ID,
+  measurementId: import.meta.env.MEASUREMENT_ID
+};
+*/
+
 const firebaseConfig = {
   apiKey: "AIzaSyDenaQpC44i30FU4_VCw-lcY2Ib57vbq5U",
   authDomain: "compsciety.firebaseapp.com",
