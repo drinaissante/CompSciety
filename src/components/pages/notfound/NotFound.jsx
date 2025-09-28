90183aff2289263920a62037e480f8ca851dce2c
drinaissante <victoriaaldrin812@gmail.com>
2025-09-28 19:08:35 +0800

    Fixed 404 error thing, just add design/content

--- src/components/pages/notfound/NotFound.jsx
+++ src/components/pages/notfound/NotFound.jsx
@@ -2,12 +2,13 @@ import Footer from "../home/sections/Footer.jsx";
 
 function NotFound() {
     return (
-        <>
-            <div className="min-h-screen bg-gradient-to-b from-[#18230F] via-[#2b5016] to-[#324d22]">
+        <div className='flex flex-col scroll-smooth'>    
+            <div className="min-h-screen bg-gradient-to-b from-[#18230F] via-[#2b5016] to-[#324d22] flex justify-center items-center text-center">
                 404 ERROR 
             </div>
+            
             <Footer />
-        </>
+        </div>
     );  
 }
 
