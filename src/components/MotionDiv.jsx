fb538bd4b051c15aeafe7ed0c851524456b8802d
drinaissante <victoriaaldrin812@gmail.com>
2025-09-29 11:07:29 +0800

    Added MotionDivExit to not confuse with MotionDiv

--- src/components/MotionDiv.jsx
+++ src/components/MotionDiv.jsx
@@ -8,20 +8,44 @@ const MotionDiv = ({
   whileInView = { opacity: 1, y: 0 },
   transition = { duration: 0.8, ease: "easeOut" },
   viewport = { once: true },
-  hasViewed = undefined,
+}) => {
+  return (
+    <motion.div
+      className={className}
+      initial={initial}
+      whileInView={whileInView}
+      transition={transition}
+      viewport={viewport}
+    >
+      {children}
+    </motion.div>
+  );
+};
+
+export const MotionDivExit = ({
+  children,
+  className = "",
+  initial = { opacity: 0, y: 50 },
+  animate = { opacity: 1, y: 0 },
+  exit = { x: -300, opacity: 0 },
+  transition = { duration: 0.8, ease: "easeOut" },
+  hasViewed = false,
+  isVisible = true, // control rendering
 }) => {
   return (
     <AnimatePresence mode="wait">
-      <motion.div
-        initial={hasViewed ? undefined : initial}
-        whileInView={whileInView}
-        transition={transition}
-        viewport={viewport}
-        exit={hasViewed ? undefined : { x: -300, opacity: 0 }}
-        className={className}
-      >
-        {children}
-      </motion.div>
+      {isVisible && (
+        <motion.div
+          className={className}
+          key="motion-exit"
+          initial={hasViewed ? false : initial} // skip initial if already viewed
+          animate={animate}
+          exit={hasViewed ? false : exit} // skip exit if already viewed
+          transition={transition}
+        >
+          {children}
+        </motion.div>
+      )}
     </AnimatePresence>
   );
 };
