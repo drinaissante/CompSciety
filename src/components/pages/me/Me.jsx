1c9d1e46072eaeef86d731dfdee284ed18c9a48e
drinaissante <victoriaaldrin812@gmail.com>
2025-10-25 19:32:54 +0800

    Base /me/ done

--- src/components/pages/me/Me.jsx
+++ src/components/pages/me/Me.jsx
@@ -1,8 +1,19 @@
 import { Link } from "react-router-dom";
 import Footer from "../home/sections/Footer.jsx";
 import MotionDiv from "../../MotionDiv.jsx";
+
 import { useEffect, useState } from "react";
-import { fetchAvatarURL, fetchProfileURL } from "../../db/database.jsx";
+
+import { authFetchAvatarURL, fetchProfileDetails } from "../../db/database.jsx";
+
+import Loading from "../../../extras/Loading.jsx";
+import ProfileImage from "../../state/ProfileImage.jsx";
+
+import { onAuthStateChanged } from "firebase/auth";
+import { auth } from "../../auth/firebase.jsx";
+
+import { IoCheckmarkCircle } from "react-icons/io5";
+import { FaClipboardList } from "react-icons/fa";
 
 /*
 - get profile picture, profile details
@@ -16,23 +27,45 @@ import { fetchAvatarURL, fetchProfileURL } from "../../db/database.jsx";
 
 
 function Me() {
-    const [profile, setProfile] = useState("");
+    const [ details, setDetails ] = useState([]);
+    const [ loading, setLoading ] = useState(true);
+
+    const [ avatarUrl, setAvatarUrl ] = useState(null);
+
+    const [ src, setSrc ] = useState(null);
 
     useEffect(() => {
         document.title = "Profile | BulSU Computer Science Society";
 
-        async function fetch() {
-            const image = await fetchProfileURL(); // returns null, no perms apparently
-            console.log(image);
-            setProfile(image);
+        async function fetchProfile() {
+            setLoading(true);
 
-            // const av = await fetchAvatarURL(username);
+            try {
+                const array = await fetchProfileDetails();
+                const avatarURL = await authFetchAvatarURL(array?.discord);
+
+                setDetails(array);
+                setSrc(<ProfileImage imageUrl={array?.profile_link} />);
+                setAvatarUrl(avatarURL);
+            } catch (error) {
+                console.error(error);
+            } finally {
+                setLoading(false);
+            }
         }
-        
-        fetch();
-    }, []);
 
-    // load the picture
+        const unsub = onAuthStateChanged(auth, async (currentUser) => {
+            if (currentUser) {
+                fetchProfile();
+            } else {
+                setDetails(null);
+            }
+        });
+
+        fetchProfile();
+
+        return unsub;
+    }, []);
 
 
     return (
@@ -42,10 +75,50 @@ function Me() {
                 <MotionDiv className="mt-25 lg:mt-35 mx-auto my-auto bg-white/40 rounded-xl gap-3">
                     
                     <div className="font-bold text-5xl text-center">Profile</div>
+
+                    {loading && <Loading />}
                     
-                    <div className="m-3 flex justify-center">
-                        <img src={profile} />
-                    </div>
+                    {src && (
+                        <>
+                            {details ? (
+                                <div className="m-3 flex flex-col text-center items-center">
+                                    {src}
+
+                                    <h1>UID: {details.uid} <FaClipboardList /></h1>  
+                                    <h1>Name: {details.name} {details.middle_initial} {details.last_name}</h1>
+
+                                    <h1 className="flex items-center justify-center gap-2">
+                                        Discord: 
+                                        <span className="flex items-center gap-2 bg-[#5865F2]/20 text-[#5865F2] px-3 py-1 rounded-full font-semibold">
+                                            <img src={avatarUrl} className="h-10 rounded-full" draggable={false}/>
+
+                                            {details.discord || "Unknown"}
+                                            {details.discord_verified && (
+                                                <div className="relative group inline-block">
+                                                    <IoCheckmarkCircle className="text-green-500 w-5 h-5 cursor-pointer" />
+                                                    
+                                                    <span
+                                                        className="absolute bottom-full mb-2 left-1/2 -translate-x-1/2 whitespace-nowrap
+                                                                bg-gray-800 text-white text-xs rounded py-1 px-2
+                                                                opacity-0 scale-90 group-hover:opacity-100 group-hover:scale-100
+                                                                transition-all duration-200 ease-out pointer-events-none shadow-lg">
+                                                        Verified Discord account
+                                                    </span>
+                                             </div>
+                                            )}
+                                            
+                                        </span>
+                                    </h1>
+
+                                    <h1>College: {details.college} | {details.program}</h1>
+                                    <h1>Year Level: {details.year_level} </h1>
+                                    <h1>Section: {details.section}</h1>
+                                </div>
+                            ) : (
+                                <Loading />
+                            )}
+                        </>
+                    )}
 
                 </MotionDiv>
             </div>
