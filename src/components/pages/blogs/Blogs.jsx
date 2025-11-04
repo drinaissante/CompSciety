a81fbef0911d61e40706d3221e49c2131641ce97
drinaissante <victoriaaldrin812@gmail.com>
2025-11-05 01:30:06 +0800

    Made blogs cover the whole screen (temp)

--- src/components/pages/blogs/Blogs.jsx
+++ src/components/pages/blogs/Blogs.jsx
@@ -3,10 +3,8 @@ import Footer from "../home/sections/Footer.jsx";
 function Blogs() {
     return (
         <div className='flex flex-col scroll-smooth bg-linear-to-tr from-[#18230F] via-[#2b5016] to-[#324d22]'>  
-            <div className=" flex text-white font-extrabold justify-center align-middle p-50 text-3xl">
+            <div className="min-h-screen flex text-white font-extrabold justify-center align-middle p-50 text-3xl">
                 Blog
-
-                
             </div>
 
             <Footer />
