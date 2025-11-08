65fd867ced42d66cf010bdf36ae45575abb43abb
drinaissante <victoriaaldrin812@gmail.com>
2025-11-08 19:01:54 +0800

    Added list of colleges and programs

---
+++ src/lib/colleges.js
@@ -0,0 +1,159 @@
+export const cafa = {
+  name: "College of Architecture and Fine Arts (CAFA)",
+  programs: [
+    "BS Architecture",
+    "B Landscape Architecture",
+    "B Fine Arts Major in Visual Communication",
+  ],
+};
+
+export const cal = {
+  name: "College of Arts and Letters (CAL)",
+  programs: [
+    "BA Broadcasting",
+    "BA Journalism",
+    "B Performing Arts (Theater Track)",
+    "B Sining sa Malikhaing Pagsulat",
+  ],
+};
+
+export const cbea = {
+  name: "College of Business Education and Accountancy (CBEA)",
+  programs: [
+    "BS Accountancy",
+    "BS Business Administration Major in Business Economics",
+    "BS Business Administration Major in Financial Management",
+    "BS Business Administration Major in Marketing Management",
+    "BS Entrepreneurship",
+  ],
+};
+
+export const ccje = {
+  name: "College of Criminal Justice Education (CCJE)",
+  programs: ["BS Criminology", "BA Legal Management"],
+};
+
+export const chtm = {
+  name: "College of Hospitality and Tourism Management (CHTM)",
+  programs: ["BS Hospitality Management", "BS Tourism Management"],
+};
+
+export const cict = {
+  name: "College of Information and Communicatinos Technology (CICT)",
+  programs: [
+    "BS Information Technology",
+    "BS Information System",
+    "B Library and Information Science",
+  ],
+};
+
+export const cit = {
+  name: "College of Industrial Technology (CIT)",
+  programs: [
+    "BIT with specialization in Automotive",
+    "BIT with specialization in Computer",
+    "BIT with specialization in Drafting",
+    "BIT with specialization in Electrical",
+    "BIT with specialization in Electronics & Communication Technology",
+    "BIT with specialization in Electronics Technology",
+    "BIT with specialization in Food Processing Technology",
+    "BIT with specialization in HVACR",
+    "BIT with specialization in Mechanical",
+    "BIT with specialization in Mechatronics Technology",
+    "BIT with specialization in Welding Technology",
+  ],
+};
+
+export const con = {
+  name: "College of Nursing (CoN)",
+  programs: ["BS Nursing"],
+};
+
+export const coe = {
+  name: "College of Engineering (CoE)",
+  programs: [
+    "BS Civil Engineering",
+    "BS Computer Engineering",
+    "BS Electrical Engineering",
+    "BS Electronics Engineering",
+    "BS Industrial Engineering",
+    "BS Manufacturing Engineering",
+    "BS Mechanical Engineering",
+    "BS Mechatronics Engineering",
+  ],
+};
+
+export const educ = {
+  name: "College of Education (EDUC)",
+  programs: [
+    "B Early Childhood Education",
+    "B Elementary Education",
+    "B Physical Education",
+    "BSEd Major in English Minor in Mandarin",
+    "BSEd in Filipino",
+    "BSEd in Mathematics",
+    "BSEd in Sciences",
+    "BSEd in Social Studies",
+    "BSEd in Values Education",
+    "BTLEd Major in Home Economics",
+    "BTLEd Major in Industrial Arts",
+    "BTLEd Major in ICT",
+  ],
+};
+
+export const cs = {
+  name: "College of Science (CS)",
+  programs: [
+    "BS Biology",
+    "BS Environmental Science",
+    "BS Food Technology",
+    "BS Math (Applied Statistics)",
+    "BS Math (Business Applications)",
+    "BS Math (Computer Science)",
+  ],
+};
+
+export const cser = {
+  name: "College of Sports, Exercise and Recreation (CSER)",
+  programs: [
+    "BS ESS (Fitness & Sports Coaching)",
+    "BS ESS (Fitness & Sports Management)",
+  ],
+};
+
+export const cssp = {
+  name: "College of Social Sciences and Philosophy (CSSP)",
+  programs: ["B Public Administration", "BS Psychology", "BS Social Work"],
+};
+
+export const colleges = [
+  cafa.name,
+  cal.name,
+  cbea.name,
+  ccje.name,
+  chtm.name,
+  cict.name,
+  cit.name,
+  con.name,
+  coe.name,
+  educ.name,
+  cs.name,
+  cser.name,
+  cssp.name,
+];
+
+export const collegeProgramsMap = {
+  [cafa.name]: cafa.programs,
+  [cal.name]: cal.programs,
+  [cbea.name]: cbea.programs,
+  [ccje.name]: ccje.programs,
+  [chtm.name]: chtm.programs,
+  [cict.name]: cict.programs,
+  [cit.name]: cit.programs,
+  [con.name]: con.programs,
+  [coe.name]: coe.programs,
+  [educ.name]: educ.programs,
+  [cs.name]: cs.programs,
+  [cser.name]: cser.programs,
+  [cssp.name]: cssp.programs,
+};
