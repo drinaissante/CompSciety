910408ad827d710439ba98cab75d30d53a5db0a0
drinaissante <victoriaaldrin812@gmail.com>
2026-02-16 21:03:16 +0800

    Added shining feet kay mascot

--- src/components/Bobbing.jsx
+++ src/components/Bobbing.jsx
@@ -2,6 +2,25 @@ import { motion } from "framer-motion";
 
 function Bobbing({logo, className}) {
    return (
+    <div className="relative inline-block">
+
+        <motion.div
+            className="absolute -bottom-8 left-1/2
+                    lg:-translate-x-1/5
+                    -translate-x-1/2
+                    w-40 h-16
+                    bg-green-400
+                    blur-2xl
+                    opacity-50
+                    rounded-full"
+            animate={{ y: [0, -12, 0] }}
+            transition={{
+                duration: 2.5,
+                repeat: Infinity,
+                ease: "easeInOut",
+            }}
+        />
+
         <motion.img
             src={logo}
             alt="CompSciety"
@@ -14,8 +33,11 @@ function Bobbing({logo, className}) {
             draggable={false}
             loading="lazy"
             decoding="async"
-            className={className}
+            className={`${className} relative z-10`}
         />
+
+    </div>
+
    )
 }
 
